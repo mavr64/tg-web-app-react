1ce39193fc171cbd555b483f6bab1f4d4e4e1302
Влад <45437497+mavr64@users.noreply.github.com>
2024-08-13 21:20:50 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+    useEffect(() => {
+        tg.ready();
+    })
+
+    const onClose = () => {
+      tg.close();
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>
+                  Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Learn React
+              </a>
+              <button onClick={onClose}>Закрыть</button>
+          </header>
+      </div>
+  );
+}
+
+export default App;
+/*
+* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +50,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+* */
